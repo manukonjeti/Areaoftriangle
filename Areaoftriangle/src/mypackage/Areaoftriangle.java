ca596a14da8628f839fdc78bd9a77261be59fae6
manukonjeti <konjetimanisha@gmail.com>
2024-11-13 21:37:26 -0500

    writing areaoftriangle

---
+++ Areaoftriangle/src/mypackage/Areaoftriangle.java
@@ -0,0 +1,20 @@
+package mypackage;
+import java.util.*;
+public class Areaoftriangle {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter base and height values");
+		
+		
+		int b= sc.nextInt();
+		
+		
+		int c=sc.nextInt();
+		int Area=(b*c)/2;
+		System.out.println("Area of triangle "+ Area);
+		
+
+	}
+
+}
